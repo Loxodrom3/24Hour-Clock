8fabc606c285f28e7a413b558e56e7d608052251
Jon Proctor <jon.proctor@phototopo.com>
2019-10-19 23:38:02 -0600

    Update oled_clock.py
    cleaned up text and labels, but still getting memory allocation errors when calling label.mpy

--- oled_clock.py
+++ oled_clock.py
@@ -1,27 +1,22 @@
-# displays time, but then errors out.
-import busio
+# import busio
 import adafruit_pcf8523
 import time
 import board
 # below are for oled
 import displayio
 import terminalio
-from adafruit_display_text import label
 import adafruit_displayio_ssd1306
+from adafruit_display_text import label
 
+font = terminalio.FONT
 displayio.release_displays()
 
 i2c = board.I2C()
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3c)
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=32)
-
-# Make the display context
-splash = displayio.Group(max_size=10)
-display.show(splash)
-# **********************
+oled = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=32)
 
 myI2C = board.I2C()  # busio.I2C(board.SCL, board.SDA)
-rtc = adafruit_pcf8523.PCF8523(myI2C)
+rtc = adafruit_pcf8523.PCF8523(i2c)
 
 days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 
@@ -31,20 +26,31 @@ if False:   # change to True if you want to write the time!
     # you must set year, mon, date, hour, min, sec and weekday
     # yearday is not supported, isdst can be set but we don't do anything with it at this time
 
-    print("Setting time to:", t)     # uncomment for debugging
-    rtc.datetime = t
-    print()
-
 while True:
-    t = rtc.datetime
+    current = rtc.datetime
     # print(t)     # uncomment for debugging
-    print("The date is %s %d/%d/%d" % (days[t.tm_wday], t.tm_mon, t.tm_mday, t.tm_year))
-    print("The time is %d:%02d:%02d" % (t.tm_hour, t.tm_min, t.tm_sec))
+    print("The date is %s %d/%d/%d" % (days[current.tm_wday], current.tm_mon, current.tm_mday, current.tm_year))
+    print("The time is %d:%02d:%02d" % (current.tm_hour, current.tm_min, current.tm_sec))
+
+    # set time and labels for display to oled
+    text_display = "Denver Time"
+    time_display = "{:d}:{:02d}:{:02d}".format(current.tm_hour, current.tm_min, current.tm_sec)
+
+    text  = label.Label(font, text=text_display)
+    clock = label.Label(font, text=time_display)
+
+    (_, _, width, _) = text.bounding_box
+    text.x = 10
+    text.y = 5
+
+    (_, _, width, _) = clock.bounding_box
+    clock.x = 10
+    clock.y = 17
     
+    clock_group = displayio.Group()
+    clock_group.append(text)
+    clock_group.append(clock)
+    
+    oled.show(clock_group)
 
-    text = ""
-    text = ("Time: %d:%02d:%02d" % (t.tm_hour, t.tm_min, t.tm_sec))
-    text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=28, y=15)
-    splash.append(text_area)
-
-    time.sleep(0.5)  # wait a second
+    time.sleep(0.9)  
